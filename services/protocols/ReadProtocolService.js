5bb230ea82b486be3e9284c1a910fdc0a4ea71f0
Filipe William <lipe.will@outlook.com>
2022-06-22 20:17:23 -0300

    Adiciona a listagem de artigos ordenados

--- services/protocols/ReadProtocolService.js
+++ services/protocols/ReadProtocolService.js
@@ -7,9 +7,15 @@ class ReadProtocolService {
         const connection = await connectToDB();
           const [articles] = await connection.execute(`SELECT * FROM protocol WHERE id = ${id};`);
           let articleData = articles[0];
-          const [rows] = await connection.execute(`SELECT * FROM article WHERE protocol = ${id};`);
 
-        return {...articleData, articles: rows};
+          let articlesList = [];
+          let [rows] = await connection.execute(`SELECT * FROM article WHERE protocolId = ${id} AND classified = 1;`);
+          articlesList = [...rows];
+
+          let otherRows = await connection.execute(`SELECT * FROM article WHERE protocolId = ${id} AND classified != 1 ORDER BY citedBy DESC;`);
+          otherRows[0].forEach(item => articlesList.push(item));
+
+        return {...articleData, articles: articlesList};
       } catch (error) {
         throw new Error(error);
       }
