629404f6cb111052262b1b5952e2c65b594a994e
Filipe William <lipe.will@outlook.com>
2022-06-22 03:30:21 -0300

    Adiciona a criação de artigos

--- services/articles/CreateArticleService.js
+++ services/articles/CreateArticleService.js
@@ -2,14 +2,11 @@ const { connectToDB } = require('../../database');
 
 class CreateArticleService {
     async execute(title, abstract, authors, year, citedBy, base, referenceUrl, protocol) {
-
-        if(!base) {
-            base = null;
-        }
+        base = (!base) ? null : base;
 
         try {
             const connection = await connectToDB();
-            const [rows] = await connection.execute(`INSERT INTO article (title, abstract, authors, year, citedBy, base, referenceUrl, protocol) VALUES (${title}, ${abstract}, ${authors}, "${year}", ${citedBy}, ${base}, ${referenceUrl}, ${protocol});`);
+            const [rows] = await connection.execute(`INSERT INTO article (title, abstract, authors, year, citedBy, reference, protocolId) VALUES ('${title}', '${abstract}', '${authors}', "${year}", ${citedBy}, '${referenceUrl}', ${protocol});`);
             return rows;
           } catch (error) {
             throw new Error(error);
